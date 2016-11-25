ace6cbd34e47df4e7bd82ca0fcc2612f0256649e
firewaterge <gyczt879574764@gmail.com>
2016-11-25 17:23:04 +0800

    Add files via upload
    美化了下界面

---
+++ index/js.js
@@ -0,0 +1,11 @@
+function SignIn_switch()
+{
+    document.getElementById('SignUp_form').style.display = 'none';
+    document.getElementById('SignIn_form').style.display = 'block';
+}
+
+function SignUp_switch()
+{
+     document.getElementById('SignIn_form').style.display = 'none';
+     document.getElementById('SignUp_form').style.display = 'block';
+}
